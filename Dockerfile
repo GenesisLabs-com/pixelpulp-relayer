d442c124d5501eb94c0681b2a7d5a4f2769dcb38
Mr. KF <khurram.farooq@rnssol.com>
2023-02-23 18:30:23 +0500

    chore: docker file updated for jenkins

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM node:16.13-slim
 
 ARG DATABASE_URL
+EXPOSE 3005
 
 WORKDIR /relayer
 ADD . /relayer
+
 RUN yarn install
 RUN yarn build
+
 CMD yarn start
--- jenkins/scripts/remote-deploy.sh
+++ jenkins/scripts/remote-deploy.sh
@@ -14,4 +14,4 @@ CONTAINER_NAME=pixelpulp-relayer
 CONTAINER_CURRENT=rnssolutions/$CONTAINER_NAME:$LABEL
     docker stop $CONTAINER_NAME-$BRANCH_NAME
     docker rm -f $CONTAINER_NAME-$BRANCH_NAME
-    docker run -v /var/pixelpulp/pixelpulp-relayer/.env:/app/.env -d -p $PORT:3005 --restart unless-stopped --name $CONTAINER_NAME-$BRANCH_NAME $CONTAINER_CURRENT
+    docker run -v /var/pixelpulp/pixelpulp-relayer/.env:/app/.env -d -p 3005:$PORT --restart unless-stopped --name $CONTAINER_NAME-$BRANCH_NAME $CONTAINER_CURRENT
