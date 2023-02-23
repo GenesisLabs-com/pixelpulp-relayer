844cc29863dc6a32584bcd2cf7f08e70d1d640e2
Mr. KF <khurram.farooq@rnssol.com>
2023-02-23 18:53:21 +0500

    chore: docker file env added for port and database url

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM node:16.13-slim
 
 ARG DATABASE_URL
+ARG PORT
 EXPOSE 3005
 
 WORKDIR /relayer
 ADD . /relayer
+
+ENV DATABASE_URL=${DATABASE_URL}
+ENV PORT=${PORT}
 
 RUN yarn install
 RUN yarn build
--- jenkins/scripts/docker-build.sh
+++ jenkins/scripts/docker-build.sh
@@ -7,4 +7,4 @@ LABEL=$(git log -1 --format=%h)
 # Build docker of current directory
 echo "Build docker image with label "$LABEL
 
-docker build -t rnssolutions/pixelpulp-relayer:$LABEL .
+docker build --build-arg PORT=$PORT --build-arg DATABASE_URL=$DATABASE_URL -t rnssolutions/pixelpulp-relayer:$LABEL .
